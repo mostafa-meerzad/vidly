e1ab386b059b641f585e50b26545554e04318a24
mostafa-meerzad <71199539+mostafa-meerzad@users.noreply.github.com>
2023-07-14 21:24:52 +0430

    add some documentation

--- hash.js
+++ hash.js
@@ -1,5 +1,7 @@
 const bcrypt = require("bcrypt");
-
+// to hash a password we need a salt which is a random string added at the beginning or end of a hashed password
+// the more the number of rounds given to genSalt the more complex the salt
+// we also need to use the original salt to decode the hashed password for authorization stuff
 async function run(){
 
     const salt = await bcrypt.genSalt(5)
--- middleware/auth.js
+++ middleware/auth.js
@@ -1,13 +1,16 @@
 const jwt = require("jsonwebtoken");
 const config = require("config");
 
+
 function auth(req, res, next) {
+
+  console.log("auth middleware called");
   const token = req.header("x-auth-token");
   if (!token) return res.status(401).send("access denied no token provided");
 
   try {
     const decode = jwt.verify(token, config.get("jwtPrivateKey"));
-    req.user = decode; // ???
+    req.user = decode; // decode has the user-id as we included earlier and we are accessing it to get the current user later in the app
     next();
   } catch (err) {
     res.status(400).send("invalid token");
