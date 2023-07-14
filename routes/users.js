a97e1cc965037483c56e3ae9d57fe46909f23ca3
mostafa-meerzad <71199539+mostafa-meerzad@users.noreply.github.com>
2023-07-14 21:24:28 +0430

    add the route to get the current user

--- routes/users.js
+++ routes/users.js
@@ -1,6 +1,6 @@
 // const mongoose = require("mongoose")
 // registering a new user
-
+const auth = require("../middleware/auth");
 const express = require("express");
 const { User, validate } = require("../models/users");
 const _ = require("lodash");
@@ -8,13 +8,22 @@ const bcrypt = require("bcrypt");
 const router = express.Router();
 const config = require("config");
 const jwt = require("jsonwebtoken");
+
+router.get("/me", auth,async (req,res)=>{
+  // we set the req.user._id in the auth middleware
+  const user = await User.findById(req.user._id).select("-password")
+  // console.log(user);
+  res.send(user)
+  res.end()
+})
+
 router.post("/", async (req, res) => {
   const { error } = validate(req.body);
   if (error) return res.status(400).send(error.details[0].message);
 
   let user = await User.findOne({ email: req.body.email });
   if (user) return res.status(400).send("user already registered");
-
+// use lodash to remove repetitive code
   //   user = new User({
   // name: req.body.name,
   // email: req.body.email,
