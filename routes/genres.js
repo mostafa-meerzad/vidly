7253c8e99f35f4e30b85433a18ae8319c93c2cc0
mostafa-meerzad <71199539+mostafa-meerzad@users.noreply.github.com>
2023-07-14 21:23:53 +0430

    bug fix and reformat genres to handle the find a genre by provided genre-id

--- routes/genres.js
+++ routes/genres.js
@@ -1,44 +1,50 @@
-const express = require("express")
-const router = express.Router()
-const {Genre, validate} = require("../models/genres")
-const auth = require("../middleware/auth")
-router.get('/', async (req, res) => {
-    const result = await Genre.find()
-    res.send(result)
-
-})
+const express = require("express");
+const router = express.Router();
+const { Genre, validate } = require("../models/genres");
+const auth = require("../middleware/auth");
+router.get("/", async (req, res) => {
+  const result = await Genre.find();
+  res.send(result);
+});
 
 router.get("/:id", async (req, res) => {
-    // find the genre
-    const result = await Genre.find({_id: req.params.id})
-    res.send(result)
-})
+  // find the genre
+  try {
+    const result = await Genre.find({ _id: req.params.id });
+    res.send(result);
+  } catch (err) {
+    res.status(400).send("invalid id");
+  }
+});
 
-router.put("/:id", async (req, res) => {
-    const {error} = validate(req.body)
-    if (error) return res.status(404).send(error.details[0].message)
-    //
-    const genre = await Genre.findByIdAndUpdate({_id: req.params.id}, {name: req.body.name})
-    if (!genre) return res.status(404).send("no such genre")
-    // genre.set({name: req.body.name})
+router.put("/:id", auth, async (req, res) => {
+  const { error } = validate(req.body);
+  if (error) return res.status(404).send(error.details[0].message);
+  //
+  const genre = await Genre.findByIdAndUpdate(
+    { _id: req.params.id },
+    { name: req.body.name }
+  );
+  if (!genre) return res.status(404).send("no such genre");
+  // genre.set({name: req.body.name})
 
-    res.send(genre)
-})
+  res.send(genre);
+});
 
 router.post("/", auth, async (req, res) => {
-    const {error} = validate(req.body)
-    if (error) return res.status(404).send(error.details[0].message)
-    let genre = new Genre({name: req.body.name})
+  const { error } = validate(req.body);
+  if (error) return res.status(404).send(error.details[0].message);
+  let genre = new Genre({ name: req.body.name });
 
-    genre = await genre.save()
+  genre = await genre.save();
 
-    res.send(genre)
-})
+  res.send(genre);
+});
 
-router.delete("/:id", async (req, res) => {
-    const genre = await Genre.findByIdAndRemove({_id: req.params.id})
+router.delete("/:id", auth, async (req, res) => {
+  const genre = await Genre.findByIdAndRemove({ _id: req.params.id });
 
-    res.send(genre)
-})
+  res.send(genre);
+});
 
-module.exports = router
+module.exports = router;
